b9b69751747e27902fba83c41d6cc7bd3b77b516
Jimmy Cheng <jimmyken793@gmail.com>
2014-03-11 16:06:01 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Nturc::Application.config.secret_key_base = '0089ec8ed541d3b1d31760339f7acf3570f198adbc7a1ac3efe2ef4fff4555ac68e42a0a94faab87819db8ef5d38948e84129bc84740478ed2c741836385f10f'
